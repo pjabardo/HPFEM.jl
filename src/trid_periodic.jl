088e54cf1c9dbe1e81e4782daa4a540890cc7bb0
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2015-11-05 12:37:22 -0200

    Removed useless file.

--- src/trid_periodic.jl
+++
@@ -1,9 +0,0 @@
-# Implements solver for periodic tridiagonal systems.
-
-type TridiagonalP{T} <: AbstractMatrix{T}
-    dl::Vector{T}
-    d::Vector{T}
-    du::Vector{T}
-    du2::Vector{T}
-    x2::Vector{T}
-end
